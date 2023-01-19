9fc94a8a0792328968a111f73c7b91f65a9c2f98
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-01-19 20:02:45 +0100

    6.4 Forum TDD; Shape collector skeleton

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.shape;
+
+public class ShapeCollector {
+    int addFigure(Shape shape) {
+        return -1;
+    }
+
+    int removeFigure(Shape shape) {
+        return -1;
+    }
+
+    Shape getFigure(int n) {
+        return null;
+    }
+
+    int showFigures() {
+        return -1;
+    }
+}
