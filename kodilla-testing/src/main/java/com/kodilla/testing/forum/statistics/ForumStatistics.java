dcc6806f329e711672c134f16ad20f9792edc2c5
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-01-21 10:18:19 +0100

    6.7 Extra task - prepared tests

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,48 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatistics {
+    private int users;
+    private int posts;
+    private int comments;
+    private double averagePostsUser;
+    private double averageCommentsUser;
+    private double averageCommentsPost;
+
+    public int getUsers() {
+        return users;
+    }
+
+    public int getPosts() {
+        return posts;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public double getAveragePostsUser() {
+        return averagePostsUser;
+    }
+
+    public double getAverageCommentsUser() {
+        return averageCommentsUser;
+    }
+
+    public double getAverageCommentsPost() {
+        return averageCommentsPost;
+    }
+
+    void calculateAdvStatistics(Statistics statistics) {
+        users = statistics.usersNames().size();
+        posts = statistics.postsCount();
+        comments = statistics.commentsCount();
+    }
+    void showStatistics() {
+        System.out.println("Users: "+users);
+        System.out.println("Posts: "+posts);
+        System.out.println("Comments: "+comments);
+        System.out.println("Average posts per user: "+averagePostsUser);
+        System.out.println("Average comments per user: "+averageCommentsUser);
+        System.out.println("Average comments per post: "+averageCommentsPost);
+    }
+}
