55be0846d49c12511d4104973cdceed50220a1e5
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-01-28 10:05:52 +0100

    7.4 World prepared test

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -2,15 +2,33 @@ package com.kodilla.stream.world;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WorldTestSuite {
     @Test
     void testGetPeopleQuantity()
     {
         //Given
+        Country china = new Country(new BigDecimal("1411750000"));
+        Country india = new Country(new BigDecimal("1375586000"));
+        Country unitedStates = new Country(new BigDecimal("334233854"));
+        Country canada = new Country(new BigDecimal("39095373"));
+        Country germany = new Country(new BigDecimal("84270625"));
+        Country france = new Country(new BigDecimal("68042591"));
 
+        Continent asia = new Continent(Set.of(china,india));
+        Continent northAmerica = new Continent(Set.of(unitedStates,canada));
+        Continent europe = new Continent(Set.of(germany,france));
+
+        World world = new World(Set.of(asia,northAmerica,europe));
         //When
-
+        BigDecimal peopleQuantity = world.getPeopleQuantity();
         //Then
-
+        BigDecimal expectedPeopleQuantity = new BigDecimal("3312978443");
+        assertEquals(peopleQuantity,expectedPeopleQuantity);
     }
 }
