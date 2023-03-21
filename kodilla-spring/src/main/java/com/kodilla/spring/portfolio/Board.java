cbb3df88beaf4a8d4a860dfa832261934ab0a410
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-03-21 12:11:39 +0100

    14.3 TaskList

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -1,8 +1,32 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Board {
+    final
     TaskList toDoList;
+    final
     TaskList inProgressList;
+    final
     TaskList doneList;
 
+    public Board(@Qualifier("toDoList") TaskList toDoList, @Qualifier("inProgressList") TaskList inProgressList, @Qualifier("doneList") TaskList doneList) {
+        this.toDoList = toDoList;
+        this.inProgressList = inProgressList;
+        this.doneList = doneList;
+    }
+
+    public TaskList getToDoList() {
+        return toDoList;
+    }
+
+    public TaskList getInProgressList() {
+        return inProgressList;
+    }
+
+    public TaskList getDoneList() {
+        return doneList;
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,12 +1,36 @@
 package com.kodilla.spring.portfolio;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class BoardTestSuite {
+    @Autowired
+    private Board board;
     @Test
     void testTaskAdd(){
         //Given
+        String task1 = "task1";
+        String task2 = "task2";
+        String task3 = "task3";
         //When
+        board.getDoneList().getTasks().add(task1);
+        board.getToDoList().getTasks().add(task2);
+        board.getInProgressList().getTasks().add(task3);
         //Then
+        assertTrue(board.getDoneList().getTasks().contains(task1));
+        assertFalse(board.getDoneList().getTasks().contains(task2));
+        assertFalse(board.getDoneList().getTasks().contains(task3));
+
+        assertTrue(board.getToDoList().getTasks().contains(task2));
+        assertFalse(board.getToDoList().getTasks().contains(task1));
+        assertFalse(board.getToDoList().getTasks().contains(task3));
+
+        assertTrue(board.getInProgressList().getTasks().contains(task3));
+        assertFalse(board.getInProgressList().getTasks().contains(task1));
+        assertFalse(board.getInProgressList().getTasks().contains(task2));
     }
 }
