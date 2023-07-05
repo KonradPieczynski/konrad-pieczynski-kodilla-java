435eee183b7230f52d10cab0aadd5cbb8799e40e
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-07-05 16:45:18 +0200

    tests

--- src/main/resources/30,1.sql
+++ src/main/resources/30,1.sql
@@ -2,23 +2,25 @@ DROP FUNCTION IF EXISTS ShowName;
 
 DELIMITER $$
 
-CREATE FUNCTION ShowName(userID INT) RETURNS VARCHAR(20) NOT DETERMINISTIC
+CREATE FUNCTION ShowName(userID INT) RETURNS VARCHAR(20) DETERMINISTIC
 BEGIN
-   DECLARE result VARCHAR(20) DEFAULT 'Wrong id';
-   DECLARE result2 VARCHAR(20) DEFAULT '';
-   IF userID > 0 THEN
-   SET result = (SELECT FIRSTNAME from readers where READER_ID = userID);
-   SET result2 = (SELECT LASTNAME from readers where READER_ID = userID);
-   IF result is NULL THEN
-   SET result = 'No such user';
-   RETURN result;
-   END IF;
-   RETURN CONCAT(result,", ",result2);
-   ELSE
-   RETURN result;
-   END IF;   
+    DECLARE result VARCHAR(20) DEFAULT 'Wrong id';
+    DECLARE result2 VARCHAR(20) DEFAULT '';
+    IF userID > 0 THEN
+        SET result = (SELECT FIRSTNAME from READERS where READER_ID = userID);
+        SET result2 = (SELECT LASTNAME from READERS where READER_ID = userID);
+        IF result is NULL THEN
+            SET result = 'No such user';
+            RETURN result;
+        END IF;
+        RETURN CONCAT(result,', ',result2);
+    ELSE
+        RETURN result;
+    END IF;
 END $$
 
 DELIMITER ;
+
+COMMIT;
 
 SELECT ShowName(1), ShowName(2), Showname (12),Showname(0);
