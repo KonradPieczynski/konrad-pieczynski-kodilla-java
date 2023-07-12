d2a47be4516b45d00d2f66dc2c83e68a4ac92fd3
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-07-12 14:59:44 +0200

    in progress

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class FacebookTestingApp {
+
+    public static final String WWW = "https://www.facebook.com";
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+
+}
