2d140c1d4f96711ccb5a314fd6de03790ab22f89
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-07-12 19:16:00 +0200

    selenium

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -12,9 +13,31 @@ import java.util.concurrent.TimeUnit;
 public class FacebookTestingApp {
 
     public static final String WWW = "https://www.facebook.com";
+    public static final String COOKIES = "//button[contains(@data-testid,\"cookie-policy-manage-dialog-accept-button\")]";
+    public static final String REGISTER = "//a[contains(@data-testid,\"open-registration-form-button\")]";
 
     public static void main(String[] args) throws InterruptedException {
-
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get(WWW);
+        while(!driver.findElement(By.xpath(COOKIES)).isDisplayed());
+        driver.findElement(By.xpath(COOKIES)).click();
+        while(!driver.findElement(By.xpath(REGISTER)).isDisplayed());
+        driver.findElement(By.xpath(REGISTER)).click();
+        while(!driver.findElement(By.name("firstname")).isDisplayed());
+        driver.findElement(By.name("firstname")).sendKeys("Test firstname");
+        driver.findElement(By.name("lastname")).sendKeys("Test firstname");
+        driver.findElement(By.name("reg_email__")).sendKeys("test@test.com");
+        driver.findElement(By.name("reg_passwd__")).sendKeys("password");
+        Select selectMonth = new Select(driver.findElement(By.name("birthday_month")));
+        selectMonth.selectByIndex(0);
+        Select selectDay = new Select(driver.findElement(By.name("birthday_day")));
+        selectDay.selectByIndex(0);
+        Select selectYear = new Select(driver.findElement(By.name("birthday_year")));
+        selectYear.selectByValue("2000");
+        driver.findElements(By.name("sex")).get(1).click();
+        while(!driver.findElement(By.name("reg_email_confirmation__")).isDisplayed());
+        driver.findElement(By.name("reg_email_confirmation__")).sendKeys("test@test.com");
+        //driver.findElement(By.name("websubmit")).click();
     }
-
 }
--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -9,23 +9,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CrudAppTestSuite {
-    private static final String BASE_URL = "https://.github.io";
+    private static final String BASE_URL = "https://konradpieczynski-tasks.github.io";
     private WebDriver driver;
 
     private Random generator;
 
-    private final String TRELLO_USERNAME = System.getenv("TRELLO_USERNAME");
+    private final String TRELLO_USERNAME = "";
 
-    private final String TRELLO_PASSWORD = System.getenv("TRELLO_PASSWORD");
+    private final String TRELLO_PASSWORD = "";
 
     @BeforeEach
     public void initTests() {
-        driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         driver.get(BASE_URL);
         generator = new Random();
     }
@@ -56,9 +57,8 @@ public class CrudAppTestSuite {
     }
 
     private void deleteCrudAppTestTask(String taskName) throws InterruptedException{
-        driver.navigate().refresh();
-
-        while(!driver.findElement(By.xpath("//select[1]")).isDisplayed());
+        driver.switchTo().alert().accept();
+        Thread.sleep(5000);
 
         driver.findElements(
                         By.xpath("//form[@class=\"datatable__row\"]")).stream()
@@ -67,7 +67,8 @@ public class CrudAppTestSuite {
                                 .getText().equals(taskName))
                 .forEach(theForm -> {
                     WebElement buttonDeleteTask =
-                            theForm.findElement(By.xpath(".//button[contains(delete)]"));
+                            //theForm.findElement(By.xpath(".//button[contains(@data-task-delete-button,\"\")]"));
+                            theForm.findElement(By.xpath(".//button[text()=\"Delete\"]"));
                     buttonDeleteTask.click();
                 });
         Thread.sleep(5000);
@@ -98,26 +99,24 @@ public class CrudAppTestSuite {
     private boolean checkTaskExistsInTrello(String taskName) throws InterruptedException {
         final String TRELLO_URL = "https://trello.com/login";
         boolean result = false;
-        WebDriver driverTrello = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        WebDriver driverTrello = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         driverTrello.get(TRELLO_URL);
 
         driverTrello.findElement(By.id("user")).sendKeys(TRELLO_USERNAME);
-        driverTrello.findElement(By.id("password")).sendKeys(TRELLO_PASSWORD);
-        WebElement el = driverTrello.findElement(By.id("login"));
-        el.submit();
+        driverTrello.findElement(By.id("login")).submit();
 
         Thread.sleep(4000);
 
         driverTrello.findElement(By.id("password")).sendKeys(TRELLO_PASSWORD);
         driverTrello.findElement(By.id("login-submit")).submit();
 
-        Thread.sleep(4000);
+        Thread.sleep(8000);
 
         driverTrello.findElements(By.xpath("//a[@class=\"board-tile\"]")).stream()
                 .filter(aHref -> aHref.findElements(By.xpath(".//div[@title=\"Kodilla Application\"]")).size() > 0)
                 .forEach(WebElement::click);
 
-        Thread.sleep(4000);
+        Thread.sleep(8000);
 
         result = driverTrello.findElements(By.xpath("//span")).stream()
                 .anyMatch(theSpan -> theSpan.getText().equals(taskName));
